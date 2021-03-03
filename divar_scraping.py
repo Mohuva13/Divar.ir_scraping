0815c6ed9f7b066c933545074a1bc056e27ca24e
Mohuva <mohuva13@gmail.com>
2021-03-03 19:44:19 +0330

    Scraping and find attrs - define and name variables

--- divar_scraping.py
+++ divar_scraping.py
@@ -28,3 +28,12 @@ try:
 except:
     try_req(the_page)
     soup = BeautifulSoup(the_page, 'html.parser')
+	
+year = soup.find_all(attrs={'class':'vehicle-card-year font-size-1'})
+price = soup.find_all('h4')
+mil = soup.find_all(attrs={'data-test':'vehicleMileage'})
+model = soup.find_all(attrs={'class':'vehicle-header-make-model text-truncate'})
+year_list = []
+price_list = []
+mil_list = []
+model_list = []
