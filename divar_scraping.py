b3b5d56b1de99c53401f1d416ea133569ffdfd80
Mohuva <mohuva13@gmail.com>
2021-03-03 19:41:21 +0330

    Request and DL web page - soup page

--- divar_scraping.py
+++ divar_scraping.py
@@ -13,3 +13,18 @@ def try_req(the_page):
         with urllib.request.urlopen(req) as response:
             the_page = response.read()
 
+try:
+    brand_input = input("Enter brand name from Truecar : ")
+    brand_input = brand_input.lower()
+    try:
+        brand_input = brand_input.replace(" ","-")
+    except:
+        pass
+    req = urllib.request.Request(f'https://www.truecar.com/used-cars-for-sale/listings/{brand_input}/')
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+    soup = BeautifulSoup(the_page, 'html.parser')
+
+except:
+    try_req(the_page)
+    soup = BeautifulSoup(the_page, 'html.parser')
