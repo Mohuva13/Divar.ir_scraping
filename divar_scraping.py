a1fd8883ecf0b609313295c2d68f5faacf32a005
Mohuva <hussein30003@gmail.com>
2021-03-03 19:38:54 +0330

    Request to divar function

--- divar_scraping.py
+++ divar_scraping.py
@@ -2,3 +2,14 @@ import mysql.connector
 import re
 from bs4 import BeautifulSoup
 import urllib.request
+
+def try_req(the_page):
+    try:
+        req = urllib.request.Request(f'https://www.truecar.com/used-cars-for-sale/listings/{brand_input}/')
+        with urllib.request.urlopen(req) as response:
+            the_page = response.read()
+    except:
+        req = urllib.request.Request(f'https://www.truecar.com/used-cars-for-sale/listings/{brand_input}/')
+        with urllib.request.urlopen(req) as response:
+            the_page = response.read()
+
